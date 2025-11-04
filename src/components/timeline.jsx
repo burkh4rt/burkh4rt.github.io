6cc52eee9f77eec5b28f617940194b3be29b3307
Michael Burkhart <burkh4rt@uchicago.edu>
2025-11-03 22:23:51 -0500

    migrate timeline styling

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -9,7 +9,7 @@ export default function TLine() {
   return (
     <Timeline
       as="section"
-      sx={{
+      style={{
         maxWidth: "calc(max(75%,500px))",
         animationName: "unfaded",
         animationDuration: "0.5s",
@@ -20,7 +20,7 @@ export default function TLine() {
         <Timeline.Badge>
           <MortarBoardIcon />
         </Timeline.Badge>
-        <Timeline.Body sx={{ fontSize: "1rem" }}>
+        <Timeline.Body style={{ fontSize: "1rem" }}>
           I earned my Ph.D. in 2019 from Brown University's Division of Applied
           Mathematics. For my{" "}
           <Link
@@ -28,12 +28,12 @@ export default function TLine() {
             target="_blank"
             rel="noopener noreferrer"
             inline={true}
-            sx={{ whiteSpace: "nowrap" }}
+            className="nowrap"
           >
             dissertation&thinsp;
             <LinkExternalIcon verticalAlign="middle" />
           </Link>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.6084/m9.figshare.8085749"
@@ -61,7 +61,7 @@ export default function TLine() {
           Clinical Trial that enables participants with quadriplegia to
           communicate and interact with their environments in real time using
           mental imagery alone{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1162/neco_a_01275"
@@ -80,12 +80,12 @@ export default function TLine() {
         <Timeline.Badge>
           <GitCommitIcon />
         </Timeline.Badge>
-        <Timeline.Body sx={{ fontSize: "1rem" }}>
+        <Timeline.Body style={{ fontSize: "1rem" }}>
           I then spent three years working as a machine learning scientist at
           Adobe in California. My main projects involved customer segmentation
           and causal inference. I also supervised intern projects in
           representation learning for semi-supervised classification{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1007/978-3-030-50420-5_22"
@@ -98,7 +98,7 @@ export default function TLine() {
             </Link>
             ,
           </Text>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             <Link
               href="https://patents.google.com/patent/US11455518B2"
               target="_blank"
@@ -111,7 +111,7 @@ export default function TLine() {
             )
           </Text>{" "}
           and causal inference{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1016/j.jocs.2023.102054"
@@ -124,7 +124,7 @@ export default function TLine() {
             </Link>
             ,
           </Text>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             <Link
               href="https://patents.google.com/patent/US20230376776A1"
               target="_blank"
@@ -136,7 +136,7 @@ export default function TLine() {
             </Link>
             ,
           </Text>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             <Link
               href="https://doi.org/10.6084/m9.figshare.24990633"
               target="_blank"
@@ -154,44 +154,44 @@ export default function TLine() {
         <Timeline.Badge>
           <GitCommitIcon />
         </Timeline.Badge>
-        <Timeline.Body sx={{ fontSize: "1rem" }}>
+        <Timeline.Body style={{ fontSize: "1rem" }}>
           In 2021, I joined Cambridge University as a research associate to
           develop machine learning-based approaches for the early diagnosis of
           neurodegenerative disease{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1002/alz.062434"
               target="_blank"
               rel="noopener noreferrer"
               inline={true}
-              sx={{ whiteSpace: "nowrap" }}
+              className="nowrap"
             >
               abstract&thinsp;
               <LinkExternalIcon verticalAlign="middle" />
             </Link>
             ,
           </Text>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             <Link
               href="https://www.c2d3.cam.ac.uk/sites/www.c2d3.cam.ac.uk/files/attachments/2023-01/michael_burkhart_-_trustworthy_ai_workshop_-_updated.pdf"
               target="_blank"
               rel="noopener noreferrer"
               inline={true}
-              sx={{ whiteSpace: "nowrap" }}
+              className="nowrap"
             >
               presentation&thinsp;
               <LinkExternalIcon verticalAlign="middle" />
             </Link>
             ,
           </Text>{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             <Link
               href="https://doi.org/10.1038/s41598-024-60914-w"
               target="_blank"
               rel="noopener noreferrer"
               inline={true}
-              sx={{ whiteSpace: "nowrap" }}
+              className="nowrap"
             >
               paper&thinsp;
               <LinkExternalIcon verticalAlign="middle" />
@@ -200,7 +200,7 @@ export default function TLine() {
           </Text>{" "}
           I also investigated how sequential inference can be applied to
           optimization{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1007/s11590-022-01895-5"
@@ -215,7 +215,7 @@ export default function TLine() {
           </Text>{" "}
           and conditions for non-coprime actions in abstract groups to have
           fixed points{" "}
-          <Text sx={{ whiteSpace: "nowrap" }}>
+          <Text className="nowrap">
             (
             <Link
               href="https://doi.org/10.1017/prm.2023.96"
@@ -245,7 +245,7 @@ export default function TLine() {
         <Timeline.Badge>
           <GitCommitIcon />
         </Timeline.Badge>
-        <Timeline.Body sx={{ fontSize: "1rem" }}>
+        <Timeline.Body style={{ fontSize: "1rem" }}>
           In 2024, I joined the{" "}
           <Link
             href="https://bbjlab.com"
