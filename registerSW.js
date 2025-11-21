3ce7299fb4b225d0a883ef7cb0a322f22cc46bee
Michael Burkhart <burkh4rt@uchicago.edu>
2025-11-21 10:35:01 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+"serviceWorker"in navigator&&window.addEventListener("load",(()=>{navigator.serviceWorker.register("/sw.js",{scope:"/"})}));
